2311863166a5c3f4c13173b4d35956fca1d157c6
Poloz Alexey <polozhev@mail.ru>
2018-03-07 12:18:03 +0300

    Pilot

---
+++ main.py
@@ -0,0 +1,35 @@
+from func.vk_user import *
+from time import sleep
+from photo import paste
+
+'''
+def parse(text):
+	text = text.replace('мы', 'они')
+
+	return text
+'''
+
+def max_size(lis):
+	q = set(lis.keys())
+	ma = 0
+	for t in q:
+		if 'photo_' in t and int(t[6:]) > ma:
+			ma = int(t[6:])
+	return lis['photo_' + str(ma)]
+
+while True:
+	for i in read():
+		image = []
+		for j in i[2]:
+			url = max_size(j['photo'])
+			image.append('data/%d-%d.jpg' % (j['photo']['owner_id'], j['photo']['id']))
+
+			with open(image[-1], 'wb') as file:
+				file.write(requests.get(url).content)
+
+			paste(image[-1], i[1])
+			image[-1] = image[-1][:-4] + '.png'
+
+		send(i[0], i[1], image)
+
+	sleep(1)
