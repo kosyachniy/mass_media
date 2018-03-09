6e9a45018a200e31866a33dca90e907283e1aa12
Poloz Alexey <polozhev@mail.ru>
2018-03-09 12:00:43 +0300

    Centered

--- photo.py
+++ photo.py
@@ -3,35 +3,28 @@ from PIL import ImageFont
 from PIL import ImageDraw 
 
 TEMPLATE = 1
-IMAGE = 1
+IMAGE = '1.jpg'
 TEXT = 'Здесь шрифт FS Joey'
 
 def paste(image, text, template=TEMPLATE):
-	img = Image.open('tpl%d.png' % TEMPLATE, 'r')
-	'''
-
-	print(img_w, img_h)
-	'''
-
+	template = Image.open('tpl%d.png' % TEMPLATE, 'r')
 	background = Image.open(image, 'r')
-	'''
-	bg_w, bg_h = background.size
-	print(bg_w, bg_h)
-	'''
+	canvas = Image.new('RGBA', template.size, (255, 255, 255, 255))
 
-	bck2 = Image.new('RGBA', img.size, (255, 255, 255, 255))
+	template_w, template_h = template.size
 
-	img_w, img_h = img.size
-	background = background.resize((int(img_w * 0.9), int(img_h * 0.85)), Image.ANTIALIAS)
+	background = background.resize((int(template_w * 0.9), int(template_h * 0.85)), Image.ANTIALIAS)
 
-	bck2.paste(background, (int(img_w * 0.1), 0))
-	bck2.paste(img, (0, 0), img)
+	canvas.paste(background, (int(template_w * 0.1), 0))
+	canvas.paste(template, (0, 0), template)
 
-	draw = ImageDraw.Draw(bck2)
+	draw = ImageDraw.Draw(canvas)
 	font = ImageFont.truetype('FS_JoeyPro-MediumRegular.otf', 60)
-	draw.text((int(0.15 * img_w), int(0.86 * img_h)), text, (0, 0, 0), font=font)
 
-	bck2.save(image[:-4]+'.png')
+	text_w, text_h = draw.textsize(text, font=font)
+	draw.text(((1.13 * template_w - text_w) // 2, int(0.86 * template_h)), text, (0, 0, 0), font=font)
+
+	canvas.save(image[:-4]+'.png')
 
 if __name__ == '__main__':
 	paste(IMAGE, TEXT, TEMPLATE)
