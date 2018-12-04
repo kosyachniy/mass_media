a31e8ec0d8c84fd81dd7eab88caf3371fdfd59b5
kosyachniy <polozhev@mail.ru>
2018-12-04 15:35:13 +0300

    Instagram

--- photo.py
+++ photo.py
@@ -1,32 +1,55 @@
 from PIL import Image, ImageFont, ImageDraw 
 
 
-def mass_media_old(template, background, text, canvas, width, height):
+def mass_media_old(template, background, text, width, height):
+	canvas = Image.new('RGBA', template.size, (255, 255, 255, 255))
 	background = background.resize((int(width * 0.9), int(height * 0.85)), Image.ANTIALIAS)
 
 	canvas.paste(background, (int(width * 0.1), 0))
 	canvas.paste(template, (0, 0), template)
 
 	draw = ImageDraw.Draw(canvas)
-	font = ImageFont.truetype('fonts/FS_JoeyPro-MediumRegular.otf', 60)
+	font = ImageFont.truetype('fonts/FS Joey Pro.otf', 60)
 
 	text_w, text_h = draw.textsize(text, font=font)
 	draw.text(((1.12 * width - text_w) // 2, int(0.87 * height)), text, font=font, fill='#ed2c2d')
 
 	return canvas
 
-TEMPLATE = 1
-PROCESS = (mass_media_old, )
+def instagram(template, background, text, width, height):
+	text = text.upper()
+
+	background.paste(template, (0, 0), template)
+
+	draw = ImageDraw.Draw(background)
+	font = ImageFont.truetype('fonts/GOST Type A.ttf', int(0.092 * height))
+
+	text_w, text_h = draw.textsize(text, font=font)
+	draw.text(((width - text_w) // 2, int(0.906 * height)), text, font=font, fill='#fff')
+
+	return background
+
+TEMPLATE = 2
+PROCESS = (mass_media_old, instagram, )
 
 
 def paste(image, text, style=TEMPLATE):
 	template = Image.open(f'templates/{style}.png', 'r')
 	background = Image.open(f'data/{image}', 'r')
-	canvas = Image.new('RGBA', template.size, (255, 255, 255, 255))
 
-	width, height = template.size
+	tmp_width, tmp_height = template.size
+	bck_width, bck_height = background.size
+	tmp_ratio = tmp_width / tmp_height
+	bck_ratio = bck_width / bck_height
 
-	canvas = PROCESS[style-1](template, background, text, canvas, width, height)
+	if tmp_ratio > bck_ratio:
+		margin = (bck_height - bck_width / tmp_ratio) // 2
+		background = background.resize(template.size, Image.ANTIALIAS, (0, margin, bck_width, bck_height-margin))
+	elif tmp_ratio < bck_ratio:
+		margin = (bck_width - bck_height * tmp_ratio) // 2
+		background = background.resize(template.size, Image.ANTIALIAS, (margin, 0, bck_width-margin, bck_height))
+
+	canvas = PROCESS[style-1](template, background, text, tmp_width, tmp_height)
 
 	name = 'results/' + image[:image.rfind('.')] + '.png'
 	canvas.save(name)
@@ -35,4 +58,4 @@ def paste(image, text, style=TEMPLATE):
 
 
 if __name__ == '__main__':
-	paste('re.jpg', 'Рандомный заголовок!', 1)
+	paste('kotek.jpg', 'рандомная новость')
